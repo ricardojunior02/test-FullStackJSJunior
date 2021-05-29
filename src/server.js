5646a07ba1ca86c0f81fb7f1d27fed1f7188f117
ricardojunior02 <ricardojunior199502@gmail.com>
2021-05-29 11:35:00 -0300

    Servidor da aplicação

---
+++ src/server.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import route from './routes';
+const app = express();
+
+app.use(express.json());
+app.use(route);
+
+app.listen(3333, () => console.log('Teste contele rodando...'))
